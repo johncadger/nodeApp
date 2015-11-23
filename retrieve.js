de08c133d01a438b02db77c1eaccdc868874fd99
1204848 <j.c.1983@live.com>
2015-11-23 15:47:55 +0000

    a

--- retrieve.js
+++ retrieve.js
@@ -27,8 +27,10 @@ http.createServer(function(request, response) {
         } else {
             //HURRAY!! We are connected. :)
             response.write('Connection established to' + url +"\n");
+
             // Get the documents collection
             var collection = db.collection('users');
+
             //We have a cursor now with our find criteria
             var results = collection.find({name: 'modulus user'});
 
@@ -39,22 +41,16 @@ http.createServer(function(request, response) {
                 } else {
                     response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
                 }
-
                 //if the result is null, there are no more results, it’s ok to close everything
                 if (result == null) {
                     response.end('Completed');
                     db.close();
                 }
-
             });
 
 
-
-
-
-            // do some work here with the database.
-
         }
+
     });
 
 }).listen(port);
