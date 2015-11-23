a21adb7613c9e82b679cb0d11c906272871603ee
1204848 <j.c.1983@live.com>
2015-11-23 13:51:18 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 1204848 on 23/11/2015.
+ */
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
