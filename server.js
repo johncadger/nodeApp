b757457ffbc1c439d6d845c98c4994179f3226fc
1204848 <j.c.1983@live.com>
2015-11-23 14:30:24 +0000

    a

--- server.js
+++ server.js
@@ -1,24 +1,4 @@
 /**
  * Created by 1204848 on 23/11/2015.
  */
-var http = require('http')
-var port = process.env.PORT || 1337;
-
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<!DOCTYPE 'html'>");
-    response.write("<html>");
-    response.write("<head>");
-    response.write("<title>Hello World Page</title>");
-    response.write("</head>");
-    response.write("<body>");
-    response.write("<h1>");
-    response.write("Hello World!");
-    response.write("</h1>");
-    response.write("Hello World!");
-    response.write("</body>");
-    response.write("</html>");
-    response.end();
-});
-server.listen(port);
-console.log("Server is listening");
+require('./connect');
