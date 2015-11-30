9dbf3a27a7712fde4cda146e7b2f2bcd60c29b7e
1204848 <j.c.1983@live.com>
2015-11-30 14:32:17 +0000

    a

--- twittertest.js
+++ twittertest.js
@@ -15,7 +15,7 @@ var client = new Twitter({
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
 
-    client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
+    client.get('search/tweets', {q: 'lolcats',count:'2'}, function(error, tweets){
         var json = [];
         for (var i =0; i< tweets.statuses.length ; i++)
         {
