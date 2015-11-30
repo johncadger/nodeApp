6b9946ac0ab29cbe529d078b9f8cc1201d2d0a6f
1204848 <j.c.1983@live.com>
2015-11-30 15:00:19 +0000

    a

--- twitterextras.js
+++ twitterextras.js
@@ -16,7 +16,7 @@ var client = new Twitter({
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
 
-    client.get('search/tweets', {q: querydata.q}, function(error, tweets){
+    client.get('search/tweets', {q: 'a'}, function(error, tweets){
         var json = [];
         for (var i =0; i< tweets.statuses.length ; i++)
         {
